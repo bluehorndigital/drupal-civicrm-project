63161d977c3ad305a08ce57ce7a84530944acf76
Matt Glaman <nmd.matt@gmail.com>
2020-10-22 20:53:07 -0500

    Expand installation tests

--- web/profiles/drupal_civicrm/tests/src/Functional/InstallTest.php
+++ web/profiles/drupal_civicrm/tests/src/Functional/InstallTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\drupal_civicrm\Functional;
 
+use Drupal\Core\Url;
+
 /**
  * Tests CiviCRM installation.
  *
@@ -9,8 +11,22 @@ namespace Drupal\Tests\drupal_civicrm\Functional;
  */
 class InstallTest extends CiviCrmTestBase {
 
+  /**
+   * Tests the installation in integrity.
+   */
   public function testInstall() {
-    $this->assertTrue(true);
+    $site_path = $this->kernel->getSitePath();
+    $this->assertTrue(file_exists($site_path . '/civicrm.settings.php'), "The civicrm.settings.php file was found in $site_path");
+    $this->assertTrue(function_exists('civicrm_api3'), 'civicrm_api() function exists.');
+
+    $user = $this->createUser([
+      'administer CiviCRM',
+      'access CiviCRM',
+      'access administration pages',
+    ]);
+    $this->drupalLogin($user);
+    $this->drupalGet(Url::fromRoute('civicrm.admin_config_civicrm'));
+    $this->assertSession()->titleEquals('CiviCRM | Drupal');
   }
 
 }
